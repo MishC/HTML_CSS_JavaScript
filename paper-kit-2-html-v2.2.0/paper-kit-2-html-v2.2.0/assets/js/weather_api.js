103d115bfef80f0ebf611f6b733c0af3911f44ef
MishC <michaela.chovancova@gmail.com>
2021-09-09 22:22:47 +0200

    fixed keyup event keypad ===13

--- paper-kit-2-html-v2.2.0/paper-kit-2-html-v2.2.0/assets/js/weather_api.js
+++ paper-kit-2-html-v2.2.0/paper-kit-2-html-v2.2.0/assets/js/weather_api.js
@@ -11,14 +11,14 @@ let weather = {
         const {icon, description} = data.weather[0];
         const {temp,humidity} = data.main;
         const {speed}  = data.wind;
-        console.log(name, icon, description, speed, temp, humidity);
         document.querySelector(".city").innerText = "Wheather in " + name + ', ' + country;
         document.querySelector(".icon_1").src = "https://openweathermap.org/img/wn/" + icon  + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "\u00B0C";
         document.querySelector(".humidity").innerText = "Humidity " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed " + speed + " km/h";
-
+        document.querySelector(".weather").classList.remove("loading");
+      
 
     },
 
@@ -26,7 +26,9 @@ search: function()  {
     this.fetchWeather(document.querySelector(".search-bar").value) ;
     console.log( this.fetchWeather(document.querySelector(".search-bar").value) ) ;
 } 
-} ;
+} 
 document.querySelector(".search svg").addEventListener("click", function (){weather.search()}  );
-document.querySelector(".search").addEventListener("keyup", function (event) {if (event.key="Enter"){weather.search()}
-}  )   ;
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {if (event.keyCode === 13){weather.search()}
+}  )  ;
+
+wheather.fetchWeather("Oslo");
