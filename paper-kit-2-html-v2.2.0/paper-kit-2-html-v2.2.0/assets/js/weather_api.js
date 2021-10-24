5c56d2f00f6e4514310104ff99f0f67a20f255b3
MishC <michaela.chovancova@gmail.com>
2021-10-24 23:21:45 +0200

    for cyclus for +6h in weather

--- paper-kit-2-html-v2.2.0/paper-kit-2-html-v2.2.0/assets/js/weather_api.js
+++ paper-kit-2-html-v2.2.0/paper-kit-2-html-v2.2.0/assets/js/weather_api.js
@@ -1,33 +1,61 @@
+function currentDay(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let currentDay = days[date.getDay()];
+
+  return currentDay;
+}
 // JavaScript source code
 let weather = {
   apiKey: "35022efb71ba6d400064d158d8238b4b",
   fetchWeather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&APPID=" +
-        this.apiKey
+      `https://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&APPID=${apiKey}`
     )
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
   displayWeather: function (data) {
-    const { name } = data;
-    const { country } = data.sys;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
+    console.log(data);
+    const { name } = data.city;
+    const { country } = data.city;
     document.querySelector(".city").innerText =
-      "Wheather in " + name + ", " + country;
-    document.querySelector(".icon_1").src =
-      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = Math.round(temp) + "\u00B0C";
-    document.querySelector(".humidity").innerText =
-      "Humidity " + humidity + "%";
-    document.querySelector(".wind").innerText = `Wind speed ${Math.round(
-      speed
-    )} m/s`;
+      "Weather in " + name + ", " + country;
+
+    for (i = 0; i < 3; i++) {
+      const { icon, description } = data.list[i].weather[0];
+      const { temp, humidity } = data.list[i].main;
+      const { speed } = data.list[i].wind;
+      const { dt_txt } = data.list[i];
+      let time = dt_txt.slice(11, 16);
+      let date = new Date(dt_txt);
+
+      let dateString = `${currentDay(date)}  ${dt_txt.slice(
+        8,
+        10
+      )}/${dt_txt.slice(5, 7)}  ${time}`;
+      document.querySelectorAll(".today")[i].innerText = dateString;
+
+      document.querySelectorAll(".icon_1")[
+        i
+      ].src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+      document.querySelectorAll(".description")[i].innerText = description;
+      document.querySelectorAll(".temp")[i].innerText =
+        Math.round(temp) + "\u00B0C";
+      document.querySelectorAll(".humidity")[i].innerText =
+        "Humidity " + humidity + "%";
+      document.querySelectorAll(".wind")[
+        i
+      ].innerText = `Wind speed ${Math.round(speed)} m/s`;
+    }
     document.querySelector(".weather").classList.remove("loading");
   },
 
