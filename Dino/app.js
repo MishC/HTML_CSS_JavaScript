70a00cfdd70eb64e6005855c9fd4d647d5c8f3df
MishC <michaela.chovancova@gmail.com>
2023-05-06 08:54:41 +0200

    Update app.js

--- Dino/app.js
+++ Dino/app.js
@@ -93,17 +93,17 @@ async function findClosestDino(weightH, heightH, dietH, data = getJSON()) {
   dinos.forEach((dino) => {
     //=> Calculate the difference between the inputted dinosaur and the current dinosaur
     if (!isNaN(weightH)) {
-      weightDiff = Math.abs(weightH - dino.weight);
+      weightDiff = Math.abs(weightH - dino.weight) * 0.1;
     } else {
       weightDiff = 1;
     }
     if (!isNaN(heightH)) {
-      heightDiff = Math.abs(heightH - dino.height);
+      heightDiff = Math.abs(heightH - dino.height) * 0.1;
     } else {
       heightDiff = 1;
     }
 
-    let dietDiff = dietH !== dino.diet ? 1 : 0;
+    let dietDiff = dietH !== dino.diet ? 10 : 0;
     let totalDiff = heightDiff + weightDiff + dietDiff;
     //=> If the current dinosaur is closer than the previous closest dinosaur, update the closest dinosaur
     if (totalDiff < closestDifference) {
