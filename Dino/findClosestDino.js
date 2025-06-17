e5fe7abee1af143eafbcf2157c237f24a4014b0e
MishC <michaela.chovancova@gmail.com>
2025-06-17 19:57:13 +0200

    changed css

--- Dino/findClosestDino.js
+++ Dino/findClosestDino.js
@@ -1,25 +1,23 @@
 //Find Closest Dino to human (YOU). Function for entering modal window
-async function findClosestDino(weightH, heightH, dietH, data = getJSON()) {
+export async function findClosestDino(weightH, heightH, dietH, data, dinoArr) {
   let closestDino = null;
   let closestDifference = Infinity;
-  let weightDiff;
-  let heightDiff;
 
   const dinos = await dinoArr(data);
   dinos.forEach((dino) => {
     //=> Calculate the difference between the inputted dinosaur and the current dinosaur
     if (!isNaN(weightH)) {
-      weightDiff = Math.abs(weightH - dino.weight) * 0.1;
+      weightDiff = Math.abs(weightH - dino.weight);
     } else {
       weightDiff = 1;
     }
     if (!isNaN(heightH)) {
-      heightDiff = Math.abs(heightH - dino.height) * 0.1;
+      heightDiff = Math.abs(heightH - dino.height);
     } else {
       heightDiff = 1;
     }
 
-    let dietDiff = dietH !== dino.diet ? 10 : 0;
+    let dietDiff = dietH !== dino.diet ? 1 : 0;
     let totalDiff = heightDiff + weightDiff + dietDiff;
     //=> If the current dinosaur is closer than the previous closest dinosaur, update the closest dinosaur
     if (totalDiff < closestDifference) {
